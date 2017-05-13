9e17226631fa01a5bfe2dd643495340e54540e36
longqinsi <longqinsi@126.com>
2017-05-13 17:34:40 +0800

    Added more methods for conveniencs

---
+++ Recursion/Delegates.cs
@@ -0,0 +1,32 @@
+﻿namespace Recursion
+{
+    public delegate LazyRef<T> Recurse<T>(T arg1);
+    public delegate LazyRef<T> Target<T>(Recurse<T> recurse, T arg1);
+    public delegate LazyRef<T> Recurse2<T>(T arg1, T arg2);
+    public delegate LazyRef<T> Target2<T>(Recurse2<T> recurse, T arg1, T arg2);
+    public delegate LazyRef<T> Recurse3<T>(T arg1, T arg2, T arg3);
+    public delegate LazyRef<T> Target3<T>(Recurse3<T> recurse, T arg1, T arg2, T arg3);
+    public delegate LazyRef<T> Recurse4<T>(T arg1, T arg2, T arg3, T arg4);
+    public delegate LazyRef<T> Target4<T>(Recurse4<T> recurse, T arg1, T arg2, T arg3, T arg4);
+    public delegate LazyRef<T> Recurse5<T>(T arg1, T arg2, T arg3, T arg4, T arg5);
+    public delegate LazyRef<T> Target5<T>(Recurse5<T> recurse, T arg1, T arg2, T arg3, T arg4, T arg5);
+    public delegate LazyRef<T> Recurse6<T>(T arg1, T arg2, T arg3, T arg4, T arg5, T arg6);
+    public delegate LazyRef<T> Target6<T>(Recurse6<T> recurse, T arg1, T arg2, T arg3, T arg4, T arg5, T arg6);
+    public delegate LazyRef<T> Recurse7<T>(T arg1, T arg2, T arg3, T arg4, T arg5, T arg6, T arg7);
+    public delegate LazyRef<T> Target7<T>(Recurse7<T> recurse, T arg1, T arg2, T arg3, T arg4, T arg5, T arg6, T arg7);
+
+    public delegate LazyRef<R> Recurse<T, R>(T arg1);
+    public delegate LazyRef<R> Target<T, R>(Recurse<T, R> recurse, T arg1);
+    public delegate LazyRef<R> Recurse<T1, T2, R>(T1 arg1, T2 arg2);
+    public delegate LazyRef<R> Target<T1, T2, R>(Recurse<T1, T2, R> recurse, T1 arg1, T2 arg2);
+    public delegate LazyRef<R> Recurse<T1, T2, T3, R>(T1 arg1, T2 arg2, T3 arg3);
+    public delegate LazyRef<R> Target<T1, T2, T3, R>(Recurse<T1, T2, T3, R> recurse, T1 arg1, T2 arg2, T3 arg3);
+    public delegate LazyRef<R> Recurse<T1, T2, T3, T4, R>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+    public delegate LazyRef<R> Target<T1, T2, T3, T4, R>(Recurse<T1, T2, T3, T4, R> recurse, T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+    public delegate LazyRef<R> Recurse<T1, T2, T3, T4, T5, R>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+    public delegate LazyRef<R> Target<T1, T2, T3, T4, T5, R>(Recurse<T1, T2, T3, T4, T5, R> recurse, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+    public delegate LazyRef<R> Recurse<T1, T2, T3, T4, T5, T6, R>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+    public delegate LazyRef<R> Target<T1, T2, T3, T4, T5, T6, R>(Recurse<T1, T2, T3, T4, T5, T6, R> recurse, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+    public delegate LazyRef<R> Recurse<T1, T2, T3, T4, T5, T6, T7, R>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+    public delegate LazyRef<R> Target<T1, T2, T3, T4, T5, T6, T7, R>(Recurse<T1, T2, T3, T4, T5, T6, T7, R> recurse, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+}
--- Recursion/Recursion.cs
+++ Recursion/Recursion.cs
@@ -6,8 +6,53 @@ namespace Recursion
 {
     public static class Recursion
     {
+        public static T Call<T>(this Target<T> target, T arg) => target.ToTailCall()(arg);
+        public static T Call<T>(this Target2<T> target, T arg1, T arg2) => target.ToTailCall()(arg1, arg2);
+        public static T Call<T>(this Target3<T> target, T arg1, T arg2, T arg3) => target.ToTailCall()(arg1, arg2, arg3);
+        public static T Call<T>(this Target4<T> target, T arg1, T arg2, T arg3, T arg4) => target.ToTailCall()(arg1, arg2, arg3, arg4);
+        public static T Call<T>(this Target5<T> target, T arg1, T arg2, T arg3, T arg4, T arg5) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5);
+        public static T Call<T>(this Target6<T> target, T arg1, T arg2, T arg3, T arg4, T arg5, T arg6) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5, arg6);
+        public static T Call<T>(this Target7<T> target, T arg1, T arg2, T arg3, T arg4, T arg5, T arg6, T arg7) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+
+        public static R Call<T, R>(this Target<T, R> target, T arg) => target.ToTailCall()(arg);
+
+        public static R Call<T1, T2, R>(this Target<T1, T2, R> target, T1 arg1, T2 arg2) => target.ToTailCall()(arg1, arg2);
+
+        public static R Call<T1, T2, T3, R>(this Target<T1, T2, T3, R> target, T1 arg1, T2 arg2, T3 arg3) => target.ToTailCall()(arg1, arg2, arg3);
+
+        public static R Call<T1, T2, T3, T4, R>(this Target<T1, T2, T3, T4, R> target, T1 arg1, T2 arg2, T3 arg3, T4 arg4) => target.ToTailCall()(arg1, arg2, arg3, arg4);
+
+        public static R Call<T1, T2, T3, T4, T5, R>(this Target<T1, T2, T3, T4, T5, R> target, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5);
+
+        public static R Call<T1, T2, T3, T4, T5, T6, R>(this Target<T1, T2, T3, T4, T5, T6, R> target, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5, arg6);
+
+        public static R Call<T1, T2, T3, T4, T5, T6, T7, R>(this Target<T1, T2, T3, T4, T5, T6, T7, R> target, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7) => target.ToTailCall()(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+
+        public static Func<T, T> ToTailCall<T>(
+            this Target<T> target) => ToTailCall<T, T>((recurse, arg) => target(_arg => recurse(_arg), arg));
+
+        public static Func<T, T, T> ToTailCall<T>(
+            this Target2<T> target) => ToTailCall<T, T, T>((recurse, arg1, arg2) => target((_arg1, _arg2) => recurse(_arg1, _arg2), arg1, arg2));
+
+        public static Func<T, T, T, T> ToTailCall<T>(
+            this Target3<T> target) => ToTailCall<T, T, T, T>((recurse, arg1, arg2, arg3) => target((_arg1, _arg2, _arg3) => recurse(_arg1, _arg2, _arg3), arg1, arg2, arg3));
+
+        public static Func<T, T, T, T, T> ToTailCall<T>(
+            this Target4<T> target) => ToTailCall<T, T, T, T, T>((recurse, arg1, arg2, arg3, arg4) => target((_arg1, _arg2, _arg3, _arg4) => recurse(_arg1, _arg2, _arg3, _arg4), arg1, arg2, arg3, arg4));
+
+        public static Func<T, T, T, T, T, T> ToTailCall<T>(
+            this Target5<T> target) => ToTailCall<T, T, T, T, T, T>((recurse, arg1, arg2, arg3, arg4, arg5) => target((_arg1, _arg2, _arg3, _arg4, _arg5) => recurse(_arg1, _arg2, _arg3, _arg4, _arg5), arg1, arg2, arg3, arg4, arg5));
+
+        public static Func<T, T, T, T, T, T, T> ToTailCall<T>(
+            this Target6<T> target) => ToTailCall<T, T, T, T, T, T, T>((recurse, arg1, arg2, arg3, arg4, arg5, arg6) => target((_arg1, _arg2, _arg3, _arg4, _arg5, _arg6) => recurse(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6), arg1, arg2, arg3, arg4, arg5, arg6));
+
+        public static Func<T, T, T, T, T, T, T, T> ToTailCall<T>(
+            this Target7<T> target) => ToTailCall<T, T, T, T, T, T, T, T>((recurse, arg1, arg2, arg3, arg4, arg5, arg6, arg7) => target((_arg1, _arg2, _arg3, _arg4, _arg5, _arg6, _arg7) => recurse(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6, _arg7), arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+
+
+
         public static Func<T, R> ToTailCall<T, R>(
-            this Func<Func<T, LazyRef<R>>, T, LazyRef<R>> target)
+            this Target<T,R> target)
         {
             var calledRef = new Ref<bool>();
             var paramRef = new Ref<T>();
@@ -45,11 +90,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, R> ToTailCall<T1, T2, R>(
-            this Func<Func<T1, T2, LazyRef<R>>, T1, T2, LazyRef<R>> target)
+            this Target<T1, T2, R> target)
         {
             R TailCall(T1 a, T2 b)
             {
-                LazyRef<R> Target(Func<(T1, T2), LazyRef<R>> func1, (T1, T2) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2), R> func1, (T1, T2) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2)
                     {
@@ -66,11 +111,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, T3, R> ToTailCall<T1, T2, T3, R>(
-            this Func<Func<T1, T2, T3, LazyRef<R>>, T1, T2, T3, LazyRef<R>> target)
+            this Target<T1, T2, T3, R> target)
         {
             R TailCall(T1 a, T2 b, T3 c)
             {
-                LazyRef<R> Target(Func<(T1, T2, T3), LazyRef<R>> func1, (T1, T2, T3) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2, T3), R> func1, (T1, T2, T3) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2, T3 arg3)
                     {
@@ -88,11 +133,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, T3, T4, R> ToTailCall<T1, T2, T3, T4, R>(
-            this Func<Func<T1, T2, T3, T4, LazyRef<R>>, T1, T2, T3, T4, LazyRef<R>> target)
+            this Target<T1, T2, T3, T4, R> target)
         {
             R TailCall(T1 a, T2 b, T3 c, T4 d)
             {
-                LazyRef<R> Target(Func<(T1, T2, T3, T4), LazyRef<R>> func1, (T1, T2, T3, T4) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2, T3, T4), R> func1, (T1, T2, T3, T4) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
                     {
@@ -110,11 +155,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, T3, T4, T5, R> ToTailCall<T1, T2, T3, T4, T5, R>(
-            this Func<Func<T1, T2, T3, T4, T5, LazyRef<R>>, T1, T2, T3, T4, T5, LazyRef<R>> target)
+            this Target<T1, T2, T3, T4, T5, R> target)
         {
             R TailCall(T1 a, T2 b, T3 c, T4 d, T5 e)
             {
-                LazyRef<R> Target(Func<(T1, T2, T3, T4, T5), LazyRef<R>> func1, (T1, T2, T3, T4, T5) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2, T3, T4, T5), R> func1, (T1, T2, T3, T4, T5) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
                     {
@@ -131,11 +176,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, T3, T4, T5, T6, R> ToTailCall<T1, T2, T3, T4, T5, T6, R>(
-            this Func<Func<T1, T2, T3, T4, T5, T6, LazyRef<R>>, T1, T2, T3, T4, T5, T6, LazyRef<R>> target)
+            this Target<T1, T2, T3, T4, T5, T6, R> target)
         {
             R TailCall(T1 a, T2 b, T3 c, T4 d, T5 e, T6 f)
             {
-                LazyRef<R> Target(Func<(T1, T2, T3, T4, T5, T6), LazyRef<R>> func1, (T1, T2, T3, T4, T5, T6) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2, T3, T4, T5, T6), R> func1, (T1, T2, T3, T4, T5, T6) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
                     {
@@ -152,11 +197,11 @@ namespace Recursion
         }
 
         public static Func<T1, T2, T3, T4, T5, T6, T7, R> ToTailCall<T1, T2, T3, T4, T5, T6, T7, R>(
-            this Func<Func<T1, T2, T3, T4, T5, T6, T7, LazyRef<R>>, T1, T2, T3, T4, T5, T6, T7, LazyRef<R>> target)
+            this Target<T1, T2, T3, T4, T5, T6, T7, R> target)
         {
             R TailCall(T1 a, T2 b, T3 c, T4 d, T5 e, T6 f, T7 g)
             {
-                LazyRef<R> Target(Func<(T1, T2, T3, T4, T5, T6, T7), LazyRef<R>> func1, (T1, T2, T3, T4, T5, T6, T7) tuple)
+                LazyRef<R> Target(Recurse<(T1, T2, T3, T4, T5, T6, T7), R> func1, (T1, T2, T3, T4, T5, T6, T7) tuple)
                 {
                     LazyRef<R> Func(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
                     {
