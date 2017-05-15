2e338b75aca9b7a871aba1965abe546ea8a56954
longqinsi <longqinsi@126.com>
2017-05-15 10:34:23 +0800

    Adds a AsLazyRef extension method to fluently convert any value of type T to LazyRef<T>

---
+++ Recursion/RecursionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion
+{
+    public static class RecursionExtensions
+    {
+        public static LazyRef<T> AsLazyRef<T>(this T value) => (LazyRef<T>)value;
+    }
+}
